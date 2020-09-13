853b237b914ebf3e645f5da15f89426ec992087b
Paul Warnick <pwarnick@ford.com>
2020-09-13 14:24:50 -0400

    Add HTTP Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,8 @@ import { ContactListComponent } from './contacts/contact-list/contact-list.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
